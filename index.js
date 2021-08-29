e5cae8d0b60e5c4adde66b94ad0f69037e48cf58
Gustavo Sampaio <sampaioglima@gmail.com>
2021-08-29 19:31:56 -0300

    feat: Exibir resultados na tela

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 main();
 
 function main() {
+    let topScreen = document.getElementsByClassName("section top");
+    let bottomScreen = document.getElementsByClassName("section bottom");
+
+    // composição da equação
     let equation = '';
+
+    // lista dos botões da calculadora
     const buttons = document.getElementsByTagName("button");
 
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
 
         button.addEventListener('click', function (event) {
+            // função que executa quando um dos botões for clicado
+
+            // identificar botão clicado
             const element = event.target;
+
+            // tipo de botão. número, operador, símbolo, etc
             const className = element.attributes[0].value;
+
+            // valor do botão
             const value = element.innerHTML;
 
+            // adiciona o valor à equação de formas diferentes dependendo do tipo
             if (className === 'number')
                 equation += value;
             else if (className === 'operator')
                 equation += ` ${value} `;
-            else if (className === 'equals')
-                console.warn('RESULTADO', eval(equation));
+            else if (className === 'equals') {
 
+                // exibe resultados na tela e no console
+                topScreen[0].innerHTML = equation;
+                bottomScreen[0].innerHTML = eval(equation);
+                console.warn('RESULTADO', eval(equation));
+            }
+
+            // para debugar
             console.log(`Valor: ${value}|Tipo: ${className}|Equação ${equation}`);
         });
     }
