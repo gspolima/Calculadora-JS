17e4a546fb3cfa6fd6268c3d7df5e0028f37268a
Gustavo Sampaio <sampaioglima@gmail.com>
2021-08-29 17:27:04 -0300

    feat: Cálculo das operações básicas

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function onClick(value) {
-    if (typeof value === 'number') {
-        console.log(value);
-    }
-    else
-        console.log(value)
-}
+main();
 
-document.getElementById('v1').value="1";
+function main() {
+    let equation = '';
+    const buttons = document.getElementsByTagName("button");
+
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+
+        button.addEventListener('click', function (event) {
+            const element = event.target;
+            const className = element.attributes[0].value;
+            const value = element.innerHTML;
+
+            if (className === 'number')
+                equation += value;
+            else if (className === 'operator')
+                equation += ` ${value} `;
+            else if (className === 'equals')
+                console.warn('RESULTADO', eval(equation));
+
+            console.log(`Valor: ${value}|Tipo: ${className}|Equação ${equation}`);
+        });
+    }
+}
