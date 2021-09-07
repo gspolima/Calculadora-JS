6bd64b17011bc94c3e2defdb5df7daf9fa5ab9f0
Gustavo Sampaio <sampaioglima@gmail.com>
2021-09-06 21:59:37 -0300

    feat: implement clear and erase

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 main();
 
 function main() {
-    let topScreen = document.getElementsByClassName("section top");
-    let bottomScreen = document.getElementsByClassName("section bottom");
+    let topScreen = document.getElementsByClassName("section top")[0];
+    let bottomScreen = document.getElementsByClassName("section bottom")[0];
 
     // composição da equação
-    let equation = '';
+    let equation = [];
+    let equationForDisplay = [];
+    let joinedMembers = '';
+    let result = 0;
 
     // lista dos botões da calculadora
     const buttons = document.getElementsByTagName("button");
@@ -20,26 +23,70 @@ function main() {
             const element = event.target;
 
             // tipo de botão. número, operador, símbolo, etc
-            const className = element.attributes["class"].value;
+            const className = element.attributes['class'].value;
 
             // valor do botão
             const value = element.innerHTML;
 
-            // adiciona o valor à equação de formas diferentes dependendo do tipo
-            if (className === 'number')
-                equation += value;
-            else if (className === 'operator')
-                equation += ` ${value} `;
-            else if (className === 'equals') {
+            // ultimo elemento da equação
+            let lastElement = equation[equation.length - 1];
 
+
+            // adiciona o valor à equação de formas diferentes dependendo do tipo
+            if (className === 'number') {
+                if (typeof lastElement === 'undefined')
+                    equation.push(`${value}`);
+                else if (!isNaN(lastElement)) {
+                    equation.pop();
+                    equation.push(lastElement += value);
+                }
+                else if (isNaN(lastElement))
+                    equation.push(value);
+            }
+
+            if (className === 'operator') {
+                equation.push(`${value}`);
+            }
+
+            if (className === 'operator power')
+            {
+                let base = lastElement;
+                equation.push()
+            }
+
+            if (className === 'erase') {
+                if (equation.length === 1) {
+                    equation = [0];
+                    topScreen.innerHTML = '';
+                }
+                else
+                    equation.pop();
+            }
+
+            equationForDisplay = [...equation];
+            joinedMembers = equationForDisplay.join(' ');
+            bottomScreen.innerHTML = joinedMembers;
+
+            if (className === 'equals') {
                 // exibe resultados na tela e no console
-                topScreen[0].innerHTML = equation;
-                bottomScreen[0].innerHTML = eval(equation);
-                console.warn('RESULTADO', eval(equation));
+                let result = eval(joinedMembers);
+                topScreen.innerHTML = joinedMembers;
+                bottomScreen.innerHTML = result;
+                console.info('RESULTADO', result);
+
+                // Reseta o array da equação e adiciona o resultado
+                equation = [result];
+            }
+
+            if (className === 'clear') {
+                equation.splice(0, equation.length);
+                topScreen.innerHTML = '';
+                result = 0;
+                bottomScreen.innerHTML = result;
             }
 
             // para debugar
-            console.log(`Valor: ${value}|Tipo: ${className}|Equação ${equation}`);
+            console.log(`Valor: ${value}| Tipo: ${className}| Equação ${equation}`);
         });
     }
 }
